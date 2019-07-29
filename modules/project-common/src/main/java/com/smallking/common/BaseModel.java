15db743bfe5d66113222f56c1864f6b02b5118a0
WangShaoXiong <130302055@qq.com>
2019-07-29 10:11:48 +0800

    修复BUG

--- modules/project-common/src/main/java/com/smallking/common/BaseModel.java
+++ modules/project-common/src/main/java/com/smallking/common/BaseModel.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
  **/
 
 @EntityListeners({EntityEventListener.class})
+@MappedSuperclass
 public class BaseModel implements Serializable {
 
     private static final long serialVersionUID = 3381061073011090142L;
