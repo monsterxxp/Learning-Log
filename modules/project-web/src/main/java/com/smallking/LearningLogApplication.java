3443b57c8b7f8b378de43c045c3e6c50f1a2a9d6
WangShaoXiong <130302055@qq.com>
2019-07-29 16:44:49 +0800

    修复maven无法打包，打包完成无法加载springboot

--- modules/project-web/src/main/java/com/smallking/LearningLogApplication.java
+++ modules/project-web/src/main/java/com/smallking/LearningLogApplication.java
@@ -2,9 +2,23 @@ package com.smallking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningLogApplication {
+public class LearningLogApplication extends SpringBootServletInitializer {
+
+    /**
+     * 1、启动类继承SpringBootServletInitializer
+     * 2、重写configure方法
+     * @description:
+     * @author: smallking
+     * @date: 2019年9月29日
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearningLogApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LearningLogApplication.class, args);
